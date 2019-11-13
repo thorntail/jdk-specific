ede26c4a01cb2222d30aa7d830369ee436c1a624
Ladislav Thon <lthon@redhat.com>
2019-11-13 11:43:42 +0100

    [THORN-2479] update JBoss Modules to 1.9.1.Final
    This is a part of Thorntail rebase on WildFly 18.

--- src/main/java/org/wildfly/swarm/jdk/specific/ClassLoaders.java
+++ src/main/java/org/wildfly/swarm/jdk/specific/ClassLoaders.java
@@ -7,7 +7,7 @@ public class ClassLoaders {
     /**
      * Returns the classloader that has access to all platform classes. On Java 8, that's {@code null}, the bootstrap
      * classloader. On Java 9+, that's {@code ClassLoader.getPlatformClassLoader()}, the platform classloader.
-     * Useful as a parent classloader when creating a new {@code ClassLoader}.
+     * Useful as a parent classloader when creating a new {@code URLClassLoader}.
      */
     public static ClassLoader getPlatformClassLoader() {
         return null;
